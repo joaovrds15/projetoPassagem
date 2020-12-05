a0d1e62d4a9dd20fc889b145b35d1bfb1f7f3ce6
joaovrds15 <joaovictorodrigues4009@gmail.com>
2020-12-05 09:16:28 -0300

    Inicio do main

--- src/Telas/TelaInicio.java
+++ src/Telas/TelaInicio.java
@@ -2,18 +2,36 @@ package Telas;
 
 import java.util.Scanner;
 import Voos.Voo;
+import Voos.VooRepository;
 
 public class TelaInicio {
 	public void abrirTela() {
 		Scanner scan = new Scanner(System.in);
 		Voo voo = new Voo();
+		VooRepository vooRep = new VooRepository();
 		String parametro;
 		
 		System.out.printf("\t\tBem Vindo!\n");
 		System.out.printf("Digite Origem:\n");
-		parametro = scan.next();
+		parametro = scan.nextLine();
+		voo.setOrigem(parametro);
 		
-		System.out.printf("%s", parametro);
+		System.out.println("Digite o destino:");
+		voo.setDestino(scan.nextLine());
+		
+		System.out.println("Digite a data do voo: dd/MM/yyyy");
+		voo.setData(scan.nextLine());
+		
+		System.out.println("Digite a quantidade de adultos:");
+		Integer adultos = scan.nextInt();
+		System.out.println("Quantidade de crianças:");
+		Integer criancas = scan.nextInt();
+		System.out.println("Digite a idade das criancas");
+		Integer idade = scan.nextInt();
+		//System.out.println(vooRep.pesquisarVoo(voo).getHorario());
+		
+		// Colocar data de volta
+		
 	
 		scan.close();
 	}
