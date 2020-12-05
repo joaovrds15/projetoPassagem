789e8cbc2e5c84bcedfc2087f328a00d16e59afc
Argwarn <75281614+Argwarn@users.noreply.github.com>
2020-12-04 21:28:35 -0300

    Add files via upload

---
+++ src/Adulto.java
@@ -0,0 +1,8 @@
+
+public class Adulto extends Pessoa{
+	public Adulto(String nome, int idade,int identificacao) {
+		super(nome,idade,identificacao);
+		//para testar se o número de crianças é igual ao pedido
+		numAdultoTeste++;
+		}
+}
