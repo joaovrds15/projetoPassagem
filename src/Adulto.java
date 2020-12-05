9ea65a8c27b0830460e6b29df12ad6fffa312e56
Argwarn <75281614+Argwarn@users.noreply.github.com>
2020-12-04 21:30:35 -0300

    Delete Adulto.java

--- src/Adulto.java
+++
@@ -1,8 +0,0 @@
-
-public class Adulto extends Pessoa{
-	public Adulto(String nome, int idade,int identificacao) {
-		super(nome,idade,identificacao);
-		//para testar se o número de crianças é igual ao pedido
-		numAdultoTeste++;
-		}
-}
